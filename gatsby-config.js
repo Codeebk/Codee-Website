744febfa3c5ea32bf6196f0b8258e9f593add4c3
Codee King <codee.king@gmail.com>
2021-10-27 22:25:40 -0500

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/images/icon.png`,
+        icon: `src/images/codee_favicon.png`,
       },
     },
   ],
